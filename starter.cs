5cb9cb79e132f98502ef26eba23659b4e3bc9d6a
Skillionx <noreply@skillionx.com>
2026-06-16 10:37:07 +0200

    Add starter code

---
+++ starter.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace CustomerManagement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welkom bij de Klantbeheer Applicatie!");
+            // Hier komt de logica voor de applicatie.
+        }
+    }
+}
